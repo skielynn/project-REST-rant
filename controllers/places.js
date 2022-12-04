75f7efe42e8c48b3b1f8abfa908775227aaf2b04
Skie <skielynn.1105945@gmail.com>
2022-12-03 22:35:05 -0800

    Form data is getting through ! + places.js help

--- controllers/places.js
+++ controllers/places.js
@@ -12,8 +12,116 @@ router.get('/', (req, res) => {
         })
 })
 
+router.post('/', (req, res) => {
+    if (req.body.pic === '') { req.body.pic = undefined }
+    if (req.body.city === '') { req.body.city = undefined }
+    if (req.body.state === '') { req.body.state = undefined }
+    db.Place.create(req.body)
+        .then(() => {
+            res.redirect('/places')
+        })
+        .catch(err => {
+            if (err && err.name == 'ValidationError') {
+                let message = 'Validation Error: '
+                for (var field in err.errors) {
+                    message += `${field} was ${err.errors[field].value}. ${err.errors[field].message}\n`
+                }
+                res.render('places/new', { message })
+            }
+            else {
+                res.render('error404')
+            }
+        })
+})
+
 router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
+router.get('/:id', (req, res) => {
+    db.Place.findOne({ _id: req.params.id })
+        .populate('comments')
+        .then(place => {
+            console.log(place.comments)
+            res.render('places/show', { place })
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
+})
+
+router.put('/:id', (req, res) => {
+    db.Place.findByIdAndUpdate(req.params.id, req.body)
+        .then(() => {
+            res.redirect(`/places/${req.params.id}`)
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
+})
+
+router.delete('/:id', (req, res) => {
+    db.Place.findByIdAndDelete(req.params.id)
+        .then(() => {
+            res.redirect('/places')
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
+})
+
+router.get('/:id/edit', (req, res) => {
+    db.Place.findById(req.params.id)
+        .then(place => {
+            res.render('places/edit', { place })
+        })
+        .catch(err => {
+            res.render('error404')
+        })
+})
+
+router.post('/:id/comment', (req, res) => {
+    console.log('post comment', req.body)
+    if (req.body.author === '') { req.body.author = undefined }
+    req.body.rant = req.body.rant ? true : false
+    db.Place.findById(req.params.id)
+        .then(place => {
+            db.Comment.create(req.body)
+                .then(comment => {
+                    place.comments.push(comment.id)
+                    place.save()
+                        .then(() => {
+                            res.redirect(`/places/${req.params.id}`)
+                        })
+                        .catch(err => {
+                            res.render('error404')
+                        })
+                })
+                .catch(err => {
+                    res.render('error404')
+                })
+        })
+        .catch(err => {
+            res.render('error404')
+        })
+})
+
+router.delete('/:id/comment/:commentId', (req, res) => {
+    db.Comment.findByIdAndDelete(req.params.commentId)
+        .then(() => {
+            console.log('Success')
+            res.redirect(`/places/${req.params.id}`)
+        })
+        .catch(err => {
+            console.log('err', err)
+            res.render('error404')
+        })
+})
+
+
 module.exports = router
+ 
+// had to use finished github code for my own understanding.. 
