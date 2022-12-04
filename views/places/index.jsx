95676d3702d4fd5af15707e495ef8aedfa7a021d
Skie <skielynn.1105945@gmail.com>
2022-12-03 22:47:34 -0800

    Show route connected

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,23 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function index (data) {
-    let placesFormatted = data.places.map((place) => {
-      return (
-        <div>
-          <h2>{place.name}</h2>
-          <img src={place.pic} alt={place.name}/>
-        </div>
-      )
-    })
-    return (
-      <Def>
-          <main>
-              <h1>PLACES INDEX PAGE</h1>
-              {placesFormatted}
-          </main>
-      </Def>
-  )
-  }
-  
+    let placesFormatted = data.places.map((place, index) => {
+        return (
+          <div className="col-sm-6">
+            <h2>
+              <a href={`/places/${index}`} >
+                {place.name}
+              </a>
+            </h2>
+            <p className="text-center">
+              {place.cuisines}
+            </p>
+            <img src={place.pic} alt={place.name} />
+            <p className="text-center">
+              Located in {place.city}, {place.state}
+            </p>
+          </div>
+        )
+      })
+      
+    }
 
 module.exports = index
